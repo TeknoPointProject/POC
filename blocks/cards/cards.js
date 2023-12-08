5be7b6f58067b2e00ea7b9db88a3e8ad1cf9e8b0
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-12-08 16:52:34 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -16,3 +16,49 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(ul);
 }
+const proxyUrl = 'https://api.allorigins.win/raw?url=';
+const apiUrl = 'https://main--idfcfirstbank--teknopointproject.hlx.live/data.json';
+
+
+
+
+fetch(proxyUrl + encodeURIComponent(apiUrl))
+  .then(response => response.json())
+  .then(data => {
+    var div = document.createElement('div');
+    div.classList.add("apidata");
+
+    if (data && data.data && Array.isArray(data.data)) {
+      data.data.forEach(item => {
+        var itemDiv = document.createElement('div');
+
+        // Loop through each property of the object
+        for (const key in item) {
+          if (item.hasOwnProperty(key)) {
+            var propertyDiv = document.createElement('div');
+
+            var sourceSpan = document.createElement('span');
+            sourceSpan.textContent = key + ": ";
+
+            var destinationSpan = document.createElement('span');
+            destinationSpan.textContent = item[key];
+
+            propertyDiv.appendChild(sourceSpan);
+            propertyDiv.appendChild(destinationSpan);
+
+            itemDiv.appendChild(propertyDiv);
+          }
+        }
+
+        div.appendChild(itemDiv);
+      });
+
+      var body = document.body;
+      body.appendChild(div);
+    } else {
+      console.error('Error: Data.data is not in the expected format');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
